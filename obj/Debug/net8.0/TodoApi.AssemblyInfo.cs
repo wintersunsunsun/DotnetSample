c7837ebc95b3c624b4d79dc7410602c8466c148b
LeoChen <ta31p969763@gmail.com>
2025-03-09 09:18:54 +0800

    feat: Generate Assets for Build and Debug

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa7fead4ac2db1be5f14e6d9cff2da180a6f52f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4e4122d99d7396772c7e2d862fedf488068818")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
